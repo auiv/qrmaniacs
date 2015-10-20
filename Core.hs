b1495a3ee952b7d6eeb1e4e47e92250d3895daa7
Paolino <paolo.veronelli@com>
2015-10-20 23:42:54 +0200

    more graph

--- Core.hs
+++ Core.hs
@@ -1,15 +1,23 @@
 
-{-# LANGUAGE  TemplateHaskell  #-}
-
+{-# LANGUAGE  TemplateHaskell, ViewPatterns, TypeFamilies  #-}
+-- | The content of a 'Graph.Node'
 module Core where
 
-import Graph (Path,Node)
+import Graph 
 import Control.Lens.TH (makeLenses)
+import Control.Lens (over,view, set)
 import Data.Bson (Value(..), Document, Field (..))
 import Data.Text (Text)
+import Data.List (sort)
+import qualified Data.Set as S
 
+------------------------------------------------
+-------- types
+------------------------------
+
+type Author = Text
 data Annotation  = Annotation {
-  _annotator :: Text,
+  _annotator :: Author,
   _subpath :: Path Text,
   _noted :: Value
   }  deriving (Show,Eq)
@@ -19,31 +27,127 @@ makeLenses ''Annotation
 
 
 data  Core  = Core  {
-  _aut :: Maybe Text, --have an author
+  _author :: Maybe Author, --have an author
   _note :: [Annotation], --have a space for annotations
   _datas :: Document -- part of Data.Bson.Value
   } deriving (Show,Eq)
 
 makeLenses ''Core
 
+type instance Lof Core = Text
+--------------------------------------
+-------- validation
+-------------------------------
+-- we only allow a subset  of bson for datas field, recursion is excluded falling back to structural
+--------------------------------
+
+--------- template acceptance ------------------
+validValue :: Value -> Bool
+
+validValue (Float _) = True
+validValue (String _) = True
+validValue (Md5 _) = True
+validValue (Bool _) = True
+validValue (UTC _) = True
+validValue (RegEx _) = True
+validValue (Int32 _) = True
+validValue (Int64 _) = True
 
 
-validDataValue :: Value -> Bool
-validDataValue (Float _) = True
-validDataValue (String _) = True
-validDataValue (Md5 _) = True
-validDataValue (Bool _) = True
-validDataValue (UTC _) = True
-validDataValue (RegEx _) = True
-validDataValue (Int32 _) = True
-validDataValue (Int64 _) = True
+validateDatas :: Document -> Bool
+validateDatas = all (validValue . value)
 
-validDocument :: Document -> Document 
-validDocument = filter (\(_ := v) -> validDataValue v)
+-------- post acceptance
 
+-- iso semantics
+matchStructure :: Document -> Document -> Bool
+matchStructure (map label -> x) (map label -> y) = sort x == sort y
+
+-- match structure and value types
+matchTyping :: Document -> Document -> Bool
+matchTyping = undefined
+
+--------------------------
+--- Node instance
+--------------------------
 
 -- | a 'Node' of 'Core' and using 'Text' as 'Path' type
-type Resource = Node Text Core
+type Resource = Node Core
+
+------------------------------------------------------------
+---------------------------- interface 
+-----------------------------------------------------------
+
+
+
+----------------
+-- cloning 
+---------------
+
+-- ? account for a  new author
+
+clone 
+  :: Author -- who asked for a clone
+  -> Resource -- root
+  -> Document -- the core of the new subresource
+  -> Path Text -- path to the label of the array of subresources
+  -> Maybe Resource -- new root, if everything was ok
+clone u n d = let 
+    -- track last author
+    ch (view author -> Just u') _ = Just u'
+    ch _ x = x
+    tc c  
+        | view datas c `matchTyping` d =  Just $ set datas d c
+        | otherwise = Nothing  
+    in 
+      -- call expand feeding in an overPath
+      expand tc $ \l f -> let 
+        -- test the author is correct for a cloning
+        g (fmap (== u) -> Just True) = f
+        g _ = const Nothing
+        in overPath 
+              ch -- track author 
+              id -- do nothing on output, as we call from top
+              Nothing -- no initial authors
+              l -- the reducted path 
+              n -- root node
+              g -- the author test
+------------------------------------------------------------------------
+
+
+----------------
+-- annotation 
+---------------
+
+-- ? too expensive schema ?
+
+annotate :: Author -> Path Text -> Annotation -> Resource -> Maybe Resource
+annotate = undefined
+correctAnnotation :: S.Set Author -> Core -> Core
+correctAnnotation vs = over note $ filter (flip S.member vs . view annotator)
+extractAuthor :: Core -> S.Set Author
+extractAuthor = maybe S.empty S.singleton . view author
+
+-------------------------------------------------------
+
+
+-------------------------
+-- ? modify a resource 
+-------------------------
+
+
+post :: Author -> Path Text -> Document -> Resource -> Maybe Resource
+post = undefined
+
+--------------------------------------------------------
+
+-----------------------------
+-- retrieve a resource
+----------------------------
+
+get :: Author -> Path Text -> Resource -> Maybe Resource
+
+----------------------------------------------------------
 
 
 
--- Graph.hs
+++ Graph.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE ViewPatterns, ScopedTypeVariables, 
-    Rank2Types, TemplateHaskell, DeriveFunctor #-}
+    Rank2Types, TemplateHaskell, DeriveFunctor, TypeFamilies, FlexibleContexts #-}
 
-module Graph (Path(..),Node(..),getNode,adjustNode,atPath) where
+module Graph where -- (Path(..),Node(..),getNode,overPath) where
 
-import Control.Lens (lens,view,set, Lens', over,mapped,_2)
+import Control.Lens (lens,view,set, Lens', over,mapped,_2, _Just,_1)
 import Control.Lens.TH (makeLenses)
 import qualified Data.Set as S
 import Lib (assocGetAndReplace)
@@ -11,21 +11,27 @@ import Data.Monoid (mappend,mconcat, Monoid)
 
 -- | recursive linear path, switching on label l and an integer for indexing l aist
 data Path l = Multi l Int (Path l) | Single l (Path l) | End deriving (Show,Eq)
- 
+
+
+
 next (Single _ t) = t
 next (Multi _ _ t) = t
 
+
+type family Lof a 
+
 -- | recursive labelled n'ary structure holding some type uniform data at each node. Subnoding is indexed by label + position
-data Node l a = Node {
+data Node a = Node {
     _core :: a,
-    _subNodes :: [(l,Node l a)],
-    _subMultiNodes ::  [(l,[Node l a])] 
-    } deriving (Show, Eq, Functor)
+    _subNodes :: [(Lof a,Node a)], -- ^ single labelled nodes, the can just be modified
+    _subMultiNodes ::  [(Lof a,[Node a])] -- ^ multiple labelled nodes, each can be modified, and they can be extended by cloning last
+    }
 
 makeLenses ''Node
 
+
 -- step a node through a path and possibly return the node and a function to accept a substitution node
-nodeAtPath :: Eq l => Path l -> Node l a -> Maybe (Node l a, Node l a -> Node l a)
+nodeAtPath :: Eq (Lof a) => Path (Lof a) -> Node a -> Maybe (Node a, Node a -> Node a)
 nodeAtPath  (Multi l i _) (Node x ss ls) = do
      (ns,fk) <- assocGetAndReplace l ls 
      (n',f) <- assocGetAndReplace i $ zip [0..] ns
@@ -36,51 +42,66 @@ nodeAtPath  (Single l  _) (Node x ss ls) = do
 
 
 -- | try to retrieve a node from parent node through a path
-getNode :: Eq l =>  Path l -> Node l a -> Maybe (Node l a)
+getNode :: Eq (Lof a) =>  Path (Lof a) -> Node a -> Maybe (Node a)
 getNode End n = Just n
 getNode p@(getNode . next -> f) n = nodeAtPath p n >>= f . fst
 
-  
--- cps action down a path
-adjustNode'
-    :: Eq l  -- required for indexing subnodes
-    => (Node l a -> Maybe (Node l a))  -- fixing action
-    -> (Maybe (Node l a) -> Maybe (Node l a)) -- cps action closing
-    -> Path l -- target path
-    -> Node l a  -- parent node
-    -> Maybe (Node l a) -- new parent node
 
-adjustNode' r z End n = z $  r n
-adjustNode' r z  p n = do
+
+-- change a node at a path feeding a context built walking down
+overPath   :: Eq (Lof a)  -- required for indexing subnodes
+    => (a -> b -> b) -- context correction
+    -> (Maybe (Node a) -> Maybe (Node a)) -- cps action closing
+    -> b -- ^ initial context
+    -> Path (Lof a) -- target path
+    -> Node a  -- parent node
+    -> (b -> Node a  -> Maybe (Node a))  -- new node computation, by actual context and old node, Nothing is abort correction
+    -> Maybe (Node a) -- new parent node
+
+overPath cctx  z ctx End n r = z $  r ctx n
+overPath cctx z ctx  p n r = do
     (n',f) <- nodeAtPath p n
-    adjustNode' r (z . fmap f) (next p) n' 
+    overPath cctx (z . fmap f) (cctx (view core n) ctx) (next p) n' r
 
--- | read and potentially substitute a node at a path
-adjustNode 
-    :: Eq l  -- required for indexing subnodes
-    => (Node l a -> Maybe (Node l a))  -- fixing
-    -> Path l -- target path
-    -> Node l a -- parent
-    -> Maybe (Node l a) -- new parent node
-adjustNode r = adjustNode' r id
+---------------------------------------------------------------
+--  graph can be only expanded horizontally and it's done by cloning the head of a multi subnode, and change the core of the cloned head 
+---------------------------------------------------------------
 
+-- splitting at the last label of a path
+initPath :: Path l -> Maybe (Path l, l)
+initPath End = Nothing
+initPath (Multi l _ End) = Nothing
+initPath (Single l End) = Just (End,l)
+initPath (Single l p) = over (_Just . _1) (Single l)  $ initPath p
+initPath (Multi l i p) = over (_Just . _1) (Multi l i)  $ initPath p
 
--- | Path parametrized 'Lens' focusing a subnode at the path, if present
--- semantic is broken on set Nothing which means no-operation and not set Nothing to a node. Incidentally it's broken on view where Nothing means not found.
+-- traveler helper type
+type Pick  a =  
+    Path (Lof a) -- ^ target path
+    -> (Node a -> Maybe (Node a)) -- ^ correcting function
+    -> Maybe (Node a) -- ^ new parent node
 
-atPath :: Eq l => Path l -> Lens' (Node l a) (Maybe (Node l a))
-atPath p = lens (getNode p) $ 
-  \n r -> maybe n id $ adjustNode (const r) p n
-
+-- we split at the last the path, pick to the splitted path asking a correction of the labelled multinode, where we clone the head and correct the cloned core
+expand :: Eq (Lof a) 
+  => (a -> Maybe a) -- new node core 
+  -> Pick  a -- the picking strategy
+  -> Path (Lof a) -- path to the label of the multisubnodes field
+  -> Maybe (Node a) -- new root
+expand tc pick (initPath -> Just (p,k))
+      = pick p $ \(Node x ls ms)  -> do
+          (n@(Node y ls' ms'):ns, fn) <- assocGetAndReplace k ms
+          y' <- tc y
+          return (Node x ls . fn $ n : Node y' ls' ms': ns)
+expand _ _ _ =  Nothing
 
 
 -- | follow all paths havesting (a -> b) and backcorrecting (b -> a -> a) each node.
-correct :: forall a b l. Monoid b => (a -> b) -> (b -> a -> a) -> Node l a -> (Node l a,b)
+correct :: forall a b l. Monoid b => (a -> b) -> (b -> a -> a) -> Node a -> (Node a,b)
 correct e c n@(Node x ss ls) = (n',e x `mappend` z) where
     n' = Node (c z x) (map (over _2 fst) ss') $ map (over (_2 .mapped) fst) ls'
-    ls' :: [(l, [(Node l a, b)])]
+    ls' :: [((Lof a), [(Node a, b)])]
     ls' =  over (mapped . _2 . mapped) (correct e c) ls 
-    ss' :: [(l, (Node l a, b))]
+    ss' :: [((Lof a), (Node a, b))]
     ss' =  over (mapped . _2 ) (correct e c) ss 
     z :: b
     z = mconcat (map (snd . snd) ss') `mappend` mconcat (mconcat $ map (map snd . snd) ls') 
